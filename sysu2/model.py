eaf7efdab07422248ba353a83fc975ad291f1b57
zswgzx <34485721+zswgzx@users.noreply.github.com>
2020-06-12 16:52:46 -0500

    Update model.py to import module that works for later version of Keras

--- sysu2/model.py
+++ sysu2/model.py
@@ -6,7 +6,7 @@ from keras.models import Model
 from keras.layers import Input, merge, Conv2D, MaxPooling2D, UpSampling2D, Cropping2D, ZeroPadding2D, BatchNormalization, Activation
 from keras.layers.merge import concatenate
 from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import apply_affine_transform
 from keras import backend as K
 K.set_image_data_format('channels_last')
 
@@ -51,11 +51,12 @@ def conv_bn_relu(nd, k=3, inputs=None):
     return relu
 
 
-def get_unet(img_shape=None, first5=True):
+def get_unet(img_shape=None, filter_size=None):
+    # dim_ordering = 'tf'
     inputs = Input(shape=img_shape)
     concat_axis = -1
 
-    if first5:
+    if filter_size:
         filters = 5
     else:
         filters = 3
@@ -134,6 +135,30 @@ def predict(imgs_2channels, rows, cols, weight_dir):
     return pred0
 
 
+def augment(x_0, x_1, y):
+    theta = (np.random.uniform(-15, 15) * np.pi) / 180.
+    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta), 0],
+                                [np.sin(theta), np.cos(theta), 0],
+                                [0, 0, 1]])
+
+    shear = np.random.uniform(-.1, .1)
+    shear_matrix = np.array([[1, -np.sin(shear), 0],
+                             [0, np.cos(shear), 0],
+                             [0, 0, 1]])
+
+    zx, zy = np.random.uniform(.9, 1.1, 2)
+    zoom_matrix = np.array([[zx, 0, 0],
+                            [0, zy, 0],
+                            [0, 0, 1]])
+
+    augmentation_matrix = np.dot(np.dot(rotation_matrix, shear_matrix), zoom_matrix)
+    x_0 = apply_affine_transform(x_0[..., np.newaxis], theta=theta, tx=0, ty=0, shear=shear, zx=zx, zy=zy, channel_axis=2)
+    x_1 = apply_affine_transform(x_1[..., np.newaxis], theta=theta, tx=0, ty=0, shear=shear, zx=zx, zy=zy, channel_axis=2)
+    y = apply_affine_transform(y[..., np.newaxis], theta=theta, tx=0, ty=0, shear=shear, zx=zx, zy=zy, channel_axis=2)
+
+    return x_0[..., 0], x_1[..., 0], y[..., 0]
+
+
 def train(images, masks, base_path, patient=0, verbose=False):
     # train single model on the training set
     samples_num = images.shape[0]
@@ -144,29 +169,20 @@ def train(images, masks, base_path, patient=0, verbose=False):
     batch_size = 30
     img_shape = (row, col, channels)
 
-    # use generator for augmentation
-    img_gen_args = dict(rotation_range=15,
-                        shear_range=3,
-                        width_shift_range=.1,
-                        height_shift_range=.1,
-                        zoom_range=.1)
-    seed = 1
-    image_gen = ImageDataGenerator(**img_gen_args)
-    mask_gen = ImageDataGenerator(**img_gen_args)
-
-    image_gen.fit(images, augment=True, seed=seed)
-    mask_gen.fit(masks, augment=True, seed=seed)
-
-    # combine generators into one which yields image and masks
-    train_gen = zip(image_gen.flow(images), mask_gen.flow(masks))
-
     model = get_unet(img_shape, True)
     current_epoch = 1
     while current_epoch <= epoch:
         print('Iter. {}/{}'.format(str(current_epoch), str(epoch)))
+        images_aug = np.zeros(images.shape, dtype=np.float32)
+        masks_aug = np.zeros(masks.shape, dtype=np.float32)
 
-        history = model.fit_generator(train_gen, steps_per_epoch=2100 / batch_size, 
-                                      epochs=1, verbose=verbose, shuffle=True)
+        for i in range(samples_num):
+            images_aug[i, ..., 0], images_aug[i, ..., 1], masks_aug[i, ..., 0] = \
+                augment(images[i, ..., 0], images[i, ..., 1], masks[i, ..., 0])
+        image = np.concatenate((images, images_aug), axis=0)
+        mask = np.concatenate((masks, masks_aug), axis=0)
+
+        history = model.fit(image, mask, batch_size=batch_size, epochs=1, verbose=verbose, shuffle=True)
         current_epoch += 1
 
         if history.history['loss'][-1] > 0.99:
